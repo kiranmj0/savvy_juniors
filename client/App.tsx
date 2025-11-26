9f162d9ec9959cdf61faa01221bd84af46ba3bfc
KiranBsol <136424486+KiranBsol@users.noreply.github.com>
2025-11-26 18:30:46 +0530

    scrolling issue fixed

--- client/App.tsx
+++ client/App.tsx
@@ -13,6 +13,7 @@ import Classes from "./pages/Classes";
 import Admissions from "./pages/Admissions";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
+import ScrollToTop from "./components/ScrollToTop";
 
 const queryClient = new QueryClient();
 
@@ -22,6 +23,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
