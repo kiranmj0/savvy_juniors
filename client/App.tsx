511833d1d1a9575505e4e52a80a94fc48ac8da08
KiranBsol <kiran.mj@bsolsystems.com>
2026-01-12 16:30:55 +0530

    routing changes

--- client/App.tsx
+++ client/App.tsx
@@ -22,7 +22,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/react-test/">
+      <BrowserRouter>
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
