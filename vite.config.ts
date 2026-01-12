a4366de80c79028e90f66b840d7f4a3fe1b0c653
KiranBsol <kiran.mj@bsolsystems.com>
2026-01-12 16:28:49 +0530

    routing issue fixed

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { createServer } from "./server";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/react-test/',
   server: {
     host: "::",
     port: 8080,
